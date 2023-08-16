3bdd2a7dbd600c2adacaf95b6fe3861dccda5b5a
Ishika <ishika.upadhyay@preciousinfosystem.com>
2023-08-16 11:39:44 +0530

    added rsepc

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
   post 'password/forgot', to: 'passwords#forgot'
   post 'password/reset', to: 'passwords#reset'
   resources :lawyer_details, except: [:destroy]
   resources :news
   resources :cases
-
+  get '/*a', to: 'application#not_found'
 
 end
